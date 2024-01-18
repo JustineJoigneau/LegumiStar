bf842d405b88106956390719fc66c7c4550d540d
bruant <antonin.bruant@ecole.ensicaen.fr>
2024-01-18 11:26:43 +0100

    camera change on tkintertest

--- fruit_veg_freshness_ai/testtkinter.py
+++ fruit_veg_freshness_ai/testtkinter.py
@@ -4,29 +4,13 @@ from PIL import ImageTk, Image
 
 selected_webcam = 0
 
-def select_webcam():
-    for i in range(3):
-        capture = cv2.VideoCapture(i)
-        _, image = capture.read()
-        cv2.imwrite("img/testcam" + str(i) , image)
-        capture.release()
-        img2 = ImageTk.PhotoImage(Image.open("img/testcam" + str(i) + ".jpg"))
-        panel.configure(image=img2)
-        panel.image = img2
+INDEX = 0
 
-        #afficher les 3 photos avec un bouton a coté de chacun avec les fonctions cami pour chaque bouton
-def Cam1():
-    return 1
 
-def Cam2():
-    return 2
 
-def Cam3():
-    return 3
-
-def prendre_photo(nom_fichier):
+def prendre_photo(nom_fichier, index_webcam):
     # Initialiser la capture vidéo
-    capture = cv2.VideoCapture(1)
+    capture = cv2.VideoCapture(index_webcam)
 
     # Lire une image depuis la webcam
     _, image = capture.read()
@@ -41,7 +25,7 @@ def prendre_photo(nom_fichier):
 
 def init_photo():
     nom_fichier_photo = "img/photo_" + "test" + ".jpg"
-    prendre_photo(nom_fichier_photo)
+    prendre_photo(nom_fichier_photo, INDEX)
     print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 
 def handle_click(event):
@@ -70,17 +54,54 @@ panel3.pack()
 
 #Faire une liste de panel pour pour acceder a chacun dans select webcam
 
+def CamIndex():
+    # vérifie les 10 premiers index et retourne une liste d'index qui sont associés à une webcam
+    index = 0
+    l = []
+    i = 4
+    while i > 0:
+        cap = cv2.VideoCapture(index)
+        if cap.read()[0]:
+            l.append(index)
+            cap.release()
+        index += 1
+        i -= 1
+    return l
+
+
+def changeWebcam(event):
+    global INDEX
+    listIndex = CamIndex()
+    for elt in listIndex:
+        if INDEX == elt and INDEX != listIndex[-1]:
+            INDEX = listIndex[listIndex.index(elt)+1]
+            break
+        else:
+            INDEX = listIndex[0]
+    print(INDEX)
+    handle_click(None)
+
+
+        
+
 
 
 buttonCSV = tk.Button(text="importer le CSV")
-buttonTP = tk.Button(text="Take Picture")
+buttonChangeWebcam = tk.Button(text="Changer la Webcam")
+buttonTP = tk.Button(text="Prendre une photo")
 
 buttonFLC = tk.Button(text="Faites le chanter")
 
 
+
+
 buttonTP.bind("<Button-1>", handle_click)
+buttonChangeWebcam.bind("<Button-1>", changeWebcam)
+buttonChangeWebcam.bind("<Button-1>", print(INDEX))
 
 buttonCSV.pack()
+buttonChangeWebcam.pack()
 buttonTP.pack()
 buttonFLC.pack()
+
 window.mainloop()
